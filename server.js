dec9d7f981b781deab0810f488ba92e4a0c77a42
Roger Acosta <roger.acosta@rocketmail.com>
2019-07-24 20:03:00 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import models from './models';
+import GraphHTTP from 'express-graphql';
+import Schema from './graphql';
+import cors from 'cors';
+
+var app = express();
+var router = express.Router();
+
+function startApp(port) {
+    app.listen(port, function() {
+        console.log('Server is listening on port ' + port);
+    });
+}
+
+models.sequelize.sync()
+    .then(function() {
+        startApp(8088);
+    })
+    .catch(function (e) {
+        throw new Error(e);
+    });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*
+ * This is here because of authentication. Auth middleware decodes the JWT token
+ * and saves its content to request.user object.
+ */
+app.use('/graphql', cors(), GraphHTTP((request) => ({
+        schema: Schema,
+        context: { user: request.user },
+        pretty: true,
+        graphiql: true
+})));
